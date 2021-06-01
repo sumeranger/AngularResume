c10b03cf64a3491929c13605e906bd25648234e4
Hank <gn01882537@gmail.com>
2021-06-02 02:03:23 +0800

    Initial Project

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    PropertyListComponent,
+    PropertyCardComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
